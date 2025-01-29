0811e1551bde1bbdbe140a44b077e845ce3ec02e
Bit131 <Bit.Mail121@gmail.com>
2025-01-29 01:14:53 +0000

    init

---
+++ Player/Scripts/state_idle.gd
@@ -0,0 +1,34 @@
+class_name State_Idle extends State
+
+@onready var walk : State = $"../Walk"
+@onready var attack: State = $"../Attack"
+
+
+#what happens when the player enters this state
+func Enter() -> void:
+	player.UpdateAnimation("idle")
+	pass
+
+#what happens when the player exits this state
+func Exit() -> void:
+	pass
+
+
+#what happens during the _process in this state
+func Process( _delta : float ) -> State:
+	if player.direction != Vector2.ZERO:
+		return walk
+	player.velocity = Vector2.ZERO
+	return null
+
+
+#what happens during the _physics_process update in this state
+func physics( _delta: float) -> State:
+	return null
+
+
+#what happens with input events in this state
+func HandleInput( _event: InputEvent ) -> State:
+	if Input.is_action_pressed("attack"):
+		return attack
+	return null

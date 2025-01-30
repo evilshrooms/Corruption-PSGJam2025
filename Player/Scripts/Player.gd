8d8f02cc6fbba31e9fe42acb78fbd9c239317a6c
Bit131 <Bit.Mail121@gmail.com>
2025-01-30 22:50:46 +0000

    hurt-box
    finished attack state, added new sprites, need to revisit to fix hurt-box collision box as it doesn't move with the character properly

--- Player/Scripts/Player.gd
+++ Player/Scripts/Player.gd
@@ -13,6 +13,7 @@ var direction : Vector2 = Vector2.ZERO
 @onready var sprite : Sprite2D = $Sprite2D
 @onready var state_machine : PlayerStateMachine = $StateMachine
 
+signal directionChanged( new_direction: Vector2 )
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -50,6 +51,7 @@ func SetDirection() -> bool:
 		return false
 	
 	cardinal_direction = new_dir
+	directionChanged.emit( new_dir )
 	sprite.scale.x = -1 if cardinal_direction == Vector2.LEFT else 1
 	return true
 
--- Player/Scripts/state_attack.gd
+++ Player/Scripts/state_attack.gd
@@ -2,26 +2,42 @@ class_name State_Attack extends State
 
 var attacking : bool = false 
 
+
+@export_range(1,20,0.4) var decelarate_speed : float = 5
+
 @onready var animation_player : AnimationPlayer = $"../../AnimationPlayer"
 @onready var idle : State = $"../Idle"
 @onready var walk : State = $"../Walk"
+@onready var hurt_box : HurtBox = $"../../Interactions/HurtBox"
 
 
 #what happens when the player enters this state
 func Enter() -> void:
 	player.UpdateAnimation("attack")
 	animation_player.animation_finished.connect( EndAttack )
+	
+	
+	
+	
+	
 	attacking = true
+	
+	
+	await get_tree().create_timer( 0.075 ).timeout
+	hurt_box.monitoring = true
 	pass
 
 #what happens when the player exits this state
 func Exit() -> void:
+	animation_player.animation_finished.disconnect( EndAttack )
+	attacking = false 
+	hurt_box.monitoring = false
 	pass
 
 
 #what happens during the _process in this state
 func Process( _delta : float ) -> State:
-	player.velocity = Vector2.ZERO
+	player.velocity -= player.velocity * decelarate_speed * _delta
 	
 	if attacking == false:
 		if player.direction == Vector2.ZERO:
